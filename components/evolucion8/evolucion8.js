5fe2256b4a694e0aca077e3ec481109d1a8bdc9f
davillatoro <davillatoro@scl.edu.gt>
2024-06-14 12:31:42 -0600

    Actualizacion_para_telefono

--- components/evolucion8/evolucion8.js
+++ components/evolucion8/evolucion8.js
@@ -9,14 +9,14 @@ DOM_evolucion8.innerHTML = `
             <div class="timeline-content">
                 <h2>Proyecto Manhattan y la Primera Era Nuclear</h2>
                 <p>Físicos como Albert Einstein y Leo Szilárd advirtieron al presidente Franklin D. Roosevelt sobre el potencial de una bomba nuclear y la posibilidad de que Alemania Nazi estuviera trabajando en un proyecto similar.</p>
-                <p>El Proyecto Manhattan fue un esfuerzo masivo que involucró a más de 130,000 personas y costó cerca de 2 mil millones de dólares (equivalente a unos 23 mil millones de dólares actuales). Los principales sitios de desarrollo fueron Los Álamos, Nuevo México; Oak Ridge, Tennessee; y Hanford, Washington.</p>
-                <span class="timeline-date">1942-1945</span>
+                <p> El Proyecto Manhattan fue un esfuerzo masivo que involucró a más de 130,000 personas y costó cerca de 2 mil millones de dólares (equivalente a unos 23 mil millones de dólares actuales). Los principales sitios de desarrollo fueron Los Álamos, Nuevo México; Oak Ridge, Tennessee; y Hanford, Washington.</p>
+                <span class="timeline-date">1942</span>
                 <img src="https://naukas.com/fx/uploads/2010/08/Einstein-y-Szilard.jpg" alt="" class="img_bomba">
             </div>
         </div>
         <div class="timeline-item">
             <div class="timeline-content">
-                <h2>Bombardeos de Hiroshima y Nagasaki</h2>
+                <h2>Hiroshima y Nagasaki</h2>
                 <p>La bomba "Little Boy", de uranio, fue lanzada sobre Hiroshima, matando instantáneamente a alrededor de 70,000 personas, con decenas de miles muriendo posteriormente debido a las heridas y la radiación.</p>
                 <p>La bomba "Fat Man", de plutonio, fue lanzada sobre Nagasaki, resultando en la muerte instantánea de aproximadamente 40,000 personas, con muchas más muertes subsecuentes.</p>
                 <span class="timeline-date">1945</span>
@@ -34,16 +34,16 @@ DOM_evolucion8.innerHTML = `
         </div>
         <div class="timeline-item">
             <div class="timeline-content">
-                <h2>Desarrollos Tecnológicos y Nuevas Estrategias</h2>
+                <h2>Desarrollos Tecnológicos</h2>
                 <p>Desarrollo de misiles balísticos intercontinentales (ICBM) y sistemas de lanzamiento submarino (SLBM), lo que permitió la capacidad de realizar un segundo ataque en caso de un ataque nuclear inicial.</p>
                 <p>Proliferación de tecnología de misiles y mejoras en la precisión y capacidad destructiva de las armas nucleares y el desarrollo de armas nucleares tácticas de menor rendimiento y más portátiles.</p>
-                <span class="timeline-date">1950-</span>
+                <span class="timeline-date">1950</span>
                 <img src="https://i0.wp.com/criptotendencia.com/wp-content/uploads/2019/05/Inteligencia-Artificial-Armas-Nucleares.jpg?fit=880%2C616&ssl=1&resize=1280%2C720" alt="">
             </div>
         </div>
         <div class="timeline-item">
             <div class="timeline-content">
-                <h2>Control de Armas y Desarme</h2>
+                <h2>Control de Armas</h2>
                 <p>La disolución de la Unión Soviética llevó a una reducción significativa del arsenal nuclear global y al establecimiento de varios tratados de control de armas, como el Tratado de Reducción de Armas Estratégicas.Iniciativas de desarme y no proliferación continuaron, aunque con desafíos significativos, como las tensiones en la península coreana y las preocupaciones sobre el programa nuclear de Irán.</p>
                 <span class="timeline-date">1991-2000</span>
                 <img src="https://vientosur.info/wp-content/uploads/spip/arton12849.jpg" alt="">
@@ -60,7 +60,7 @@ DOM_evolucion8.innerHTML = `
         <div class="timeline-item">
             <div class="timeline-content">
                 <h2>Conclusión</h2>
-                <p>La evolución de las armas nucleares refleja tanto el progreso científico como los desafíos geopolíticos. A lo largo de las décadas, la comunidad internacional ha trabajado para mitigar los riesgos asociados con estas armas, aunque persisten desafíos significativos.</p>
+                <p>La evolución de las armas nucleares refleja tanto el progreso científico como los desafíos geopolíticos. A lo largo de las décadas, la comunidad internacional ha trabajado para mitigar los riesgos asociados con estas armas, aunque persisten desafíos significativos</p>
             </div>
         </div>
     </div>
